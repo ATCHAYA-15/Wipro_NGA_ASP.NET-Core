ac127bd174af578f0855004829d78f40b78d23db
Atchaya S <atchaya1564@gmail.com>
2025-08-26 10:08:49 +0530

    Add files via upload

---
+++ Repos/Dept.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using Repos.Repos;
+using System;
+using System.Collections.Generic;
+using RepoContextNamespace;
+
+namespace Repos
+{
+    public class Dept : IDept
+    {
+        private readonly RepoContext _context;
+
+        public Dept(RepoContext context)
+        {
+            _context = context;
+        }
+
+        public string AddDept(Department dept)
+        {
+            try
+            {
+                _context.Departments.Add(dept);
+                _context.SaveChanges();
+                return $" Successful addition of department!";
+            }
+            catch (Exception ex)
+            {
+                return "Department Addition failed with error: " + ex.Message;
+            }
+        }
+
+        public string EditDept(Department dept)
+        {
+            try
+            {
+                _context.Departments.Update(dept);
+                _context.SaveChanges();
+                return "Department updated successfully!";
+            }
+            catch (Exception ex)
+            {
+                return "Department Update failed: " + ex.Message;
+            }
+        }
+
+        public List<Department> GetDepartment()
+        {
+            try
+            {
+                return _context.Departments.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error fetching departments: " + ex.Message);
+            }
+        }
+
+        public string RemoveDept(int id)
+        {
+            try
+            {
+                var dept = _context.Departments.Find(id);
+                if (dept == null)
+                {
+                    return "Department not found!";
+                }
+
+                _context.Departments.Remove(dept);
+                _context.SaveChanges();
+                return "Department removed successfully!";
+            }
+            catch (Exception ex)
+            {
+                return "Department deletion failed: " + ex.Message;
+            }
+        }
+    }
+}
---
+++ Repos/IDept.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repos.Repos
+{
+    public interface IDept
+    {
+        string AddDept(Department dept);
+
+        string EditDept(Department dept);
+        List<Department> GetDepartment();
+
+        string RemoveDept(int id);
+    }
+}
